437d29d05c17fe57f9e270c0ef490bd560867cff
le-huynh <lehuynh200194@gmail.com>
2023-09-15 20:46:02 +0700

    #1: headfirst, 3E: chap 12

---
+++ head_first/e3_update_tables.py
@@ -0,0 +1,68 @@
+import os
+import DBcm
+import pyprojroot as here
+
+db_details = "CouchDB.sqlite3"
+FD = "data/swimdata/"
+
+files = os.listdir(here.here(FD))
+files.remove(".DS_Store")
+
+SQL_SELECT_SWIMMERS = """
+    select * from swimmers
+    where name = ? and age = ?
+"""
+SQL_INSERT_SWIMMERS = """
+    insert into swimmers
+    (name, age)
+    values
+    (?, ?)
+"""
+SQL_SELECT_EVENTS = """
+    select * from events
+    where distance = ? and stroke = ?
+"""
+SQL_INSERT_EVENTS = """
+    insert into events
+    (distance, stroke)
+    values
+    (?, ?)
+"""
+SQL_GET_SWIMMER = """
+    select id from swimmers
+    where name = ? and age = ?
+"""
+SQL_GET_EVENT = """
+    select id from events
+    where distance = ? and stroke = ?
+"""
+SQL_INSERT_TIMES = """
+    insert into times
+    (swimmer_id, event_id, time)
+    values
+    (?, ?, ?)
+"""
+
+with DBcm.UseDatabase(db_details) as db:
+    for fn in files:
+        name, age, distance, stroke = fn.removesuffix(".txt").split("-")
+
+        db.execute(SQL_SELECT_SWIMMERS, (name, age))
+        if not db.fetchall():
+            db.execute(SQL_INSERT_SWIMMERS, (name, age))
+        
+        db.execute(SQL_SELECT_EVENTS, (distance, stroke))
+        if not db.fetchall():
+            db.execute(SQL_INSERT_EVENTS, (distance, stroke))
+        
+        db.execute(SQL_GET_SWIMMER, (name, age))
+        s_id = db.fetchone()[0]
+
+        db.execute(SQL_GET_EVENT, (distance, stroke))
+        e_id = db.fetchone()[0]
+
+        with open(here.here(FD + fn)) as sf:
+            times = sf.read().strip().split(",")
+            for t in times:
+                db.execute(SQL_INSERT_TIMES, (s_id, e_id, t,))
+    
